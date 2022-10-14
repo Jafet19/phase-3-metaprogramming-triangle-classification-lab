905dda650c47432ea99a72d82e9a6523b090664c
Jafet Reyes Jr <jafet_19@Jafets-MacBook-Pro.local>
2022-10-14 15:24:37 -0500

    commit tree a3469e476df698ab0c2fea0d9d1de22e6032830d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
   # write code here
+  atr_accessor :
+
+  def initialize(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+
+  class TriangleError < StandardError
+  end
 end
